d60f4f41faf6d0d3fb16e74ac046c67be0780124
lcw3176 <lcw3176@naver.com>
2021-09-20 17:45:28 +0900

    뷰 매칭

--- app/src/main/java/com/joebrooks/strawberry_android/MainActivity.java
+++ app/src/main/java/com/joebrooks/strawberry_android/MainActivity.java
@@ -1,13 +1,16 @@
 package com.joebrooks.strawberry_android;
 
-import androidx.annotation.NonNull;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.joebrooks.strawberry_android.services.FileService;
+import com.joebrooks.strawberry_android.views.ChartFragment;
+import com.joebrooks.strawberry_android.views.PlayListFragment;
 import com.joebrooks.strawberry_android.views.SearchFragment;
 import com.joebrooks.strawberry_android.views.SongFragment;
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
     private Fragment songFragment;
     private Fragment searchFragment;
+    private Fragment chartFragment;
+    private Fragment playListFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         songFragment = new SongFragment();
         searchFragment = new SearchFragment();
+        chartFragment = new ChartFragment();
+        playListFragment = new PlayListFragment();
 
         // 바텀 네비게이션
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -44,15 +51,27 @@ public class MainActivity extends AppCompatActivity {
                     getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,searchFragment).commitAllowingStateLoss();
                     break;
                 case R.id.chart:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment_group).commitAllowingStateLoss();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,chartFragment).commitAllowingStateLoss();
                     break;
                 case R.id.playList:
-//                        getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,fragment_hotel).commitAllowingStateLoss();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_layout,playListFragment).commitAllowingStateLoss();
                     break;
                 default:
                     break;
             }
             return true;
         });
+
+        checkVerify();
+    }
+
+    public void checkVerify() {
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, 1);
+        }
     }
 }
