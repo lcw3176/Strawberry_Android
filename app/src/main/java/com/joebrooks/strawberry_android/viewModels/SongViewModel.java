e7c34729e7adca108f3fa59c98c90131be0d640e
lcw3176 <lcw3176@naver.com>
2021-09-20 21:31:51 +0900

    테스트

--- app/src/main/java/com/joebrooks/strawberry_android/viewModels/SongViewModel.java
+++ app/src/main/java/com/joebrooks/strawberry_android/viewModels/SongViewModel.java
@@ -3,10 +3,22 @@ package com.joebrooks.strawberry_android.viewModels;
 import androidx.databinding.ObservableField;
 import androidx.lifecycle.ViewModel;
 
+import com.joebrooks.strawberry_android.models.Song;
+import com.joebrooks.strawberry_android.services.FileService;
+
+import java.util.List;
+
 public class SongViewModel extends ViewModel {
+    public ObservableField<Song> song = new ObservableField<>();
     public ObservableField<String> test = new ObservableField<>("hello");
     private int count = 0;
+    private final FileService fileService = new FileService();
 
+    public SongViewModel(){
+        for(Song i : fileService.readAllSong()){
+            song.set(i);
+        }
+    }
     public void onClickButton(){
         test.set(String.valueOf(count++));
     }
