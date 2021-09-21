e246237d681c837de98e64d18bb2f7bf6f4e1b6f
joebrooks <lcw3176@naver.com>
2021-09-21 13:25:24 +0900

    테스트 코드 삭제

--- app/src/main/java/com/joebrooks/strawberry_android/viewModels/SongViewModel.java
+++ app/src/main/java/com/joebrooks/strawberry_android/viewModels/SongViewModel.java
@@ -6,12 +6,9 @@ import androidx.lifecycle.ViewModel;
 import com.joebrooks.strawberry_android.models.Song;
 import com.joebrooks.strawberry_android.services.FileService;
 
-import java.util.List;
-
 public class SongViewModel extends ViewModel {
     public ObservableField<Song> song = new ObservableField<>();
-    public ObservableField<String> test = new ObservableField<>("hello");
-    private int count = 0;
+
     private final FileService fileService = new FileService();
 
     public SongViewModel(){
@@ -19,8 +16,4 @@ public class SongViewModel extends ViewModel {
             song.set(i);
         }
     }
-    public void onClickButton(){
-        test.set(String.valueOf(count++));
-    }
-    // TODO: Implement the ViewModel
 }
