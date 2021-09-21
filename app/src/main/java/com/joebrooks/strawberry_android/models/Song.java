da36284e1ff31da4edba67f0c049ba67a1d53e41
joebrooks <lcw3176@naver.com>
2021-09-21 13:24:55 +0900

    id 추가

--- app/src/main/java/com/joebrooks/strawberry_android/models/Song.java
+++ app/src/main/java/com/joebrooks/strawberry_android/models/Song.java
@@ -3,8 +3,17 @@ package com.joebrooks.strawberry_android.models;
 import android.graphics.Bitmap;
 
 public class Song {
+    private String id;
     private String name;
     private Bitmap thumbnail;
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String getId(){
+        return this.id;
+    }
 
     public void setName(String name){
         this.name = name;
