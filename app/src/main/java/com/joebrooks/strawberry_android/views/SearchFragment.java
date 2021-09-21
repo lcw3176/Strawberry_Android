84083de398069559c83158f19cf136d00465ad65
joebrooks <lcw3176@naver.com>
2021-09-21 13:24:05 +0900

    setListView 추가

--- app/src/main/java/com/joebrooks/strawberry_android/views/SearchFragment.java
+++ app/src/main/java/com/joebrooks/strawberry_android/views/SearchFragment.java
@@ -1,6 +1,10 @@
 package com.joebrooks.strawberry_android.views;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -9,14 +13,10 @@ import androidx.databinding.ViewDataBinding;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.joebrooks.strawberry_android.BR;
 import com.joebrooks.strawberry_android.R;
+import com.joebrooks.strawberry_android.adapter.ListViewAdapter;
 import com.joebrooks.strawberry_android.viewModels.SearchViewModel;
-import com.joebrooks.strawberry_android.viewModels.SongViewModel;
 
 public class SearchFragment extends Fragment {
     private SearchViewModel viewModel;
@@ -31,6 +31,11 @@ public class SearchFragment extends Fragment {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false);
         binding.setLifecycleOwner(this.getViewLifecycleOwner());
         binding.setVariable(BR.viewModel, viewModel);
+
+        ListViewAdapter adapter = new ListViewAdapter();
+        ListView listview = (ListView)binding.getRoot().findViewById(R.id.searchListView);
+        listview.setAdapter(adapter);
+        viewModel.setListView(listview);
 
         return binding.getRoot();
     }
